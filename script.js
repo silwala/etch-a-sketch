dc224ef866cf5a50a0de5e6706dd26cf72acc852
Aditya Silwal <silwala@sheridancollege.ca>
2024-06-06 05:10:16 -0400

    add functionality to the slider to change grid size

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const containerWidth = 600;
 const container = document.querySelector('#container');
 const colors = document.querySelectorAll('.color');
 const colorWheel = document.querySelector('#color-wheel');
@@ -10,6 +11,8 @@ const gridSizeValue = document.querySelector('#grid-size-value');
 
 function init(){
     let brushColor = colorWheel.value;
+
+    changeGridSizeText();
 
     colorPicker.addEventListener('click', () => {
         colorWheel.click();
@@ -30,15 +33,10 @@ function init(){
         })
     }
 
-    for(let i = 0; i < 16 * 16; i++){
-        let squareDiv = document.createElement('div');
-        squareDiv.setAttribute('class', 'square-div');
-        container.appendChild(squareDiv);
-        console.log('created');
-    
-    }
+    changeGridSize(gridSize.value);
+
     draw.addEventListener('click', () => brushColor = colorWheel.value);
-    erase.addEventListener('click', () => brushColor = 'ffffff');
+    erase.addEventListener('click', () => brushColor = '#ffffff');
     clear.addEventListener('click', () => {
         for (const child of container.childNodes) {
             child.style.backgroundColor = '#ffffff'
@@ -46,7 +44,32 @@ function init(){
     });
     
     container.addEventListener('mouseover', (e) => e.target.style.backgroundColor = brushColor);
+    gridSize.addEventListener('change',() => {
+        changeGridSizeText();
+        changeGridSize(gridSize.value)
+    })
+    gridSize.addEventListener('mousemove', changeGridSizeText)
 }
+
+function changeGridSize(size){
+    console.log('called');
+    container.textContent = ''
+    for(let i = 0; i < size * size; i++){
+        let squareDiv = document.createElement('div');
+        squareDiv.setAttribute('class', 'square-div');
+        squareDiv.style.flexBasis = `calc(${containerWidth}px / ${size})`
+        squareDiv.style.height = `calc(${containerWidth}px / ${size})`
+        container.appendChild(squareDiv);
+        console.log('created');  
+    }
+}
+
+function changeGridSizeText(){
+    gridSizeValue.textContent = `${gridSize.value}x${gridSize.value}`;
+}
+
+init();
+
 
 
 
