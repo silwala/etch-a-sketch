9dd6a0172fc17c64a3eca309aa9d3be88ab3db65
Aditya Silwal <silwala@sheridancollege.ca>
2024-06-06 04:32:19 -0400

    move all the initialization steps to init function

--- script.js
+++ script.js
@@ -8,42 +8,51 @@ const clear = document.querySelector('#clear');
 const gridSize = document.querySelector('#grid-size');
 const gridSizeValue = document.querySelector('#grid-size-value');
 
-let brushColor = colorWheel.value;
+function init(){
+    let brushColor = colorWheel.value;
 
-colorPicker.addEventListener('click', () => {
-    colorWheel.click();
-});
+    colorPicker.addEventListener('click', () => {
+        colorWheel.click();
+    });
 
-colorWheel.addEventListener('change', () => {
-    colorPicker.style.backgroundColor = colorWheel.value;
-    brushColor = colorWheel.value;
-})
-
-for (const color of colors) {
-    color.style.backgroundColor = color.id;
-
-    color.addEventListener('click', () => {
-        colorPicker.style.backgroundColor = color.id;
-        colorWheel.value = color.id
-        brushColor = color.id;
+    colorWheel.addEventListener('change', () => {
+        colorPicker.style.backgroundColor = colorWheel.value;
+        brushColor = colorWheel.value;
     })
-}
 
-for(let i = 0; i < 16 * 16; i++){
-    let squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class', 'square-div');
-    container.appendChild(squareDiv);
-    console.log('created');
-
-}
-
-draw.addEventListener('click', () => brushColor = colorWheel.value);
-erase.addEventListener('click', () => brushColor = 'ffffff');
-clear.addEventListener('click', () => {
-    for (const child of container.childNodes) {
-        child.style.backgroundColor = '#ffffff'
+    for (const color of colors) {
+        color.style.backgroundColor = color.id;
+    
+        color.addEventListener('click', () => {
+            colorPicker.style.backgroundColor = color.id;
+            colorWheel.value = color.id
+            brushColor = color.id;
+        })
     }
-});
 
-container.addEventListener('mouseover', (e) => e.target.style.backgroundColor = brushColor);
+    for(let i = 0; i < 16 * 16; i++){
+        let squareDiv = document.createElement('div');
+        squareDiv.setAttribute('class', 'square-div');
+        container.appendChild(squareDiv);
+        console.log('created');
+    
+    }
+    draw.addEventListener('click', () => brushColor = colorWheel.value);
+    erase.addEventListener('click', () => brushColor = 'ffffff');
+    clear.addEventListener('click', () => {
+        for (const child of container.childNodes) {
+            child.style.backgroundColor = '#ffffff'
+        }
+    });
+    
+    container.addEventListener('mouseover', (e) => e.target.style.backgroundColor = brushColor);
+}
+
+
+
+
+
+
+
+
 
